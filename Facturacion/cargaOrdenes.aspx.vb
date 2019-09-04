b888b56f2730e5e3386ffa1372d2193909dc1a78
Fernando M Zamudio C <fzamudio@outlook.com>
2019-09-04 16:47:55 -0400

    Cliente Smart

--- Facturacion/cargaOrdenes.aspx.vb
+++ Facturacion/cargaOrdenes.aspx.vb
@@ -8,42 +8,25 @@ Public Class cargaOrdenes
     Dim total As Long
     Dim filename As String
 
+
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_Previsualizar.Click
         filename = Server.MapPath("~/App_Data/CSV/") + Path.GetFileName(FileUpload1.PostedFile.FileName).ToString
         FileUpload1.SaveAs(filename)
-        fnc_CargaCSV()
+        fnc_CargaArchivoCSVtoGRID()
 
     End Sub
     Protected Sub btn_CargarDatos_Click(sender As Object, e As EventArgs) Handles btn_CargarDatos.Click
-        Dim cargaCSV As New mmpLibrerias.Facturacion
-
-        filename = lbl_Archivo.Text
-
-        dtsOrden = cargaCSV.fnc_CargaCSV(filename)
-        For i = 0 To dtsOrden.Tables(0).Rows.Count - 1
-            fnc_CargaCliente(i)
-        Next
-
-
-        GridView1.DataSource = dtsOrden
-        GridView1.DataBind()
-
-
-
+        fnc_ExportaCVStoDTS()
     End Sub
 
 
-#Region "Metodos"
-    Private Sub sub_habilitaBotones()
-        btn_clientes.Enabled = True
-        btn_CargarDatos.Enabled = True
-    End Sub
-    Private Function fnc_CargaCSV() As Boolean
+#Region "Metodos Capa Negocio"
+    Private Function fnc_CargaArchivoCSVtoGRID() As Boolean
         Dim cargaCSV As New mmpLibrerias.Facturacion
 
         filename = Server.MapPath("~/App_Data/CSV/") + Path.GetFileName(FileUpload1.PostedFile.FileName).ToString
 
-        dtsOrden = cargaCSV.fnc_CargaCSV(filename)
+        dtsOrden = cargaCSV.fnc_CargaCSVtoDTS(filename)
         If IsNothing(dtsOrden) = False Then
 
             GridView1.DataSource = dtsOrden
@@ -57,32 +40,52 @@ Public Class cargaOrdenes
         End If
         GridView1.DataBind()
     End Function
-    Private Function fnc_CargaCliente(_pos As Long)
+    Private Function fnc_ExportaCVStoDTS() As Boolean
+        Dim cargaCSV As New mmpLibrerias.Facturacion
+
+        filename = lbl_Archivo.Text
+        Using _cnx As New SqlClient.SqlConnection(cnxMaster)
+            _cnx.Open()
+            dtsOrden = cargaCSV.fnc_CargaCSVtoDTS(filename)
+            For i = 0 To dtsOrden.Tables(0).Rows.Count - 1
+                fnc_AgregaClientetoDB(i, _cnx)
+            Next
+            _cnx.Close()
+            _cnx.Close()
+        End Using
+
+        GridView1.DataSource = dtsOrden
+        GridView1.DataBind()
+        Return True
+    End Function
+#End Region
+
+#Region "Capa Datos"
+    Private Function fnc_AgregaGeneraltoDB(_pos As Long, _cnxMasterDB As SqlClient.SqlConnection, _TablaOBJ As Object, _tabla As String)
         With dtsOrden.Tables(0).Rows(_pos)
-            Dim _cliente As New empresa_cliente(cnxMaster, gbl_empresaID, .Item("UserID"), .Item("Comprador"), 2, "cliente_direccionfacturacion_txt", "Direccion envio", "Telefono", .Item("email"))
-            Dim _ResultadoGeneral() As String = Split(_cliente.fnc_InsertaClientes(), "|")
-            Dim _Entro As Boolean = False
+            'Dim _cliente As New empresa_cliente(cnxMaster, gbl_empresaID, .Item("UserID"), .Item("Comprador"), 2, "cliente_direccionfacturacion_txt", "Direccion envio", "Telefono", .Item("email"))
+            Dim _ResultadoGeneral() As String = Split(_TablaOBJ.fnc_Inserta(_cnxMasterDB), "|")
 
             Try
                 If _ResultadoGeneral(0) = "1" Then
                     buenos = buenos + 1
-                    dtsOrden.Tables(0).Rows(_pos)("Estado") = "OK"
-                    dtsOrden.Tables(0).Rows(_pos)("Observaciones") = "Cliente Creado"
+                    fnc_ValidaGrid(_pos, _tabla, "Creado", "OK")
                     Exit Function
                 End If
 
                 If _ResultadoGeneral(0) = "2601" Then
-                    _ResultadoGeneral = Split(_cliente.fnc_ActualizaClientes(), "|")
+                    _ResultadoGeneral = Split(_TablaOBJ.fnc_Actualiza(_cnxMasterDB), "|")
                     If _ResultadoGeneral(0) = "1" Then
                         buenos = buenos + 1
-                        dtsOrden.Tables(0).Rows(_pos)("Estado") = "OK"
-                        dtsOrden.Tables(0).Rows(_pos)("Observaciones") = "Cliente Actualizado"
+                        fnc_ValidaGrid(_pos, _tabla, "Actualizado", "OK")
                         Exit Function
                     End If
+                    fnc_ValidaGrid(_pos, _tabla, _ResultadoGeneral(1), "Error")
+                Else
+                    fnc_ValidaGrid(_pos, _tabla, _ResultadoGeneral(1), "Error")
                 End If
 
-                dtsOrden.Tables(0).Rows(_pos)("Estado") = "Error"
-                dtsOrden.Tables(0).Rows(_pos)("Observaciones") = _ResultadoGeneral(1)
+                dtsOrden.Tables(0).Rows(_pos)("EstadoGeneral") = "Error"
             Catch ex As Exception
                 dtsOrden.Tables(0).Rows(_pos)("Estado") = "Error"
                 dtsOrden.Tables(0).Rows(_pos)("Observaciones") = _ResultadoGeneral(1)
@@ -96,8 +99,72 @@ Public Class cargaOrdenes
 
 
     End Function
+
+
+
+    Private Function fnc_AgregaClientetoDB(_pos As Long, _cnxMasterDB As SqlClient.SqlConnection)
+        With dtsOrden.Tables(0).Rows(_pos)
+            Dim _cliente As New empresa_cliente(cnxMaster, gbl_empresaID, .Item("UserID"), .Item("Comprador"), 2, "cliente_direccionfacturacion_txt", "Direccion envio", "Telefono", .Item("email"))
+            fnc_AgregaGeneraltoDB(_pos, _cnxMasterDB, _cliente, "Cliente")
+        End With
+
+
+        'With dtsOrden.Tables(0).Rows(_pos)
+        '    Dim _cliente As New empresa_cliente(cnxMaster, gbl_empresaID, .Item("UserID"), .Item("Comprador"), 2, "cliente_direccionfacturacion_txt", "Direccion envio", "Telefono", .Item("email"))
+        '    Dim _ResultadoGeneral() As String = Split(_cliente.fnc_InsertaClientes(_cnxMasterDB), "|")
+
+        '    Try
+        '        If _ResultadoGeneral(0) = "1" Then
+        '            buenos = buenos + 1
+        '            fnc_ValidaGrid(_pos, "Cliente", "Creado", "OK")
+        '            Exit Function
+        '        End If
+
+        '        If _ResultadoGeneral(0) = "2601" Then
+        '            _ResultadoGeneral = Split(_cliente.fnc_ActualizaClientes(_cnxMasterDB), "|")
+        '            If _ResultadoGeneral(0) = "1" Then
+        '                buenos = buenos + 1
+        '                fnc_ValidaGrid(_pos, "Cliente", "Actualizado", "OK")
+        '                Exit Function
+        '            End If
+        '        End If
+
+        '        dtsOrden.Tables(0).Rows(_pos)("EstadoGeneral") = "Error"
+        '        fnc_ValidaGrid(_pos, "Cliente", "Creado", "Error")
+        '    Catch ex As Exception
+        '        dtsOrden.Tables(0).Rows(_pos)("Estado") = "Error"
+        '        dtsOrden.Tables(0).Rows(_pos)("Observaciones") = _ResultadoGeneral(1)
+        '    End Try
+
+
+        'End With
+
+        '    btn_clientes.Text = "Clientes Cargados(" & buenos & ") - Error(" & total - buenos & ")"
+
+
+
+    End Function
 #End Region
 
+#Region "Metodos Capa Presentacion"
+    Private Sub sub_habilitaBotones()
+        btn_clientes.Enabled = True
+        btn_CargarDatos.Enabled = True
+    End Sub
 
+    Private Function fnc_ValidaGrid(_pos As Long, _Campo As String, _Accion As String, _Resultado As String) As Boolean
+        Try
+            dtsOrden.Tables(0).Rows(_pos)("Estado" & _Campo) = _Resultado
+            dtsOrden.Tables(0).Rows(_pos)("Observaciones" & _Campo) = _Accion
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+
+
+
+#End Region
 
 End Class
--- Migrations/201909031648049_Crea_SP.vb
+++ Migrations/201909031648049_Crea_SP.vb
@@ -7,22 +7,15 @@ Namespace Migrations
         Inherits DbMigration
 
         Public Overrides Sub Up()
-            CreateStoredProcedure("dbo.Movie_Insert", Function(p) New With {Key .Name = p.String(maxLength:=50), Key .ReleaseDate = p.DateTime(), Key .Category = p.String()
-    }, body:="INSERT [dbo].[Movies]([Name], [ReleaseDate], [Category2])  
-                              VALUES (@Name, @ReleaseDate, @Category)  
+            CreateStoredProcedure("dbo.empresa_cliente_UpdateByCodigo", Function(p) New With {Key .cliente_id = p.Long, .empresa_id = p.Long, .cliente_codigo_c25 = p.String(maxLength:=25), .cliente_nombre_nom = p.String(maxLength:=25), .pais_id = p.Long, .cliente_direccionfacturacion_txt = p.String(), .cliente_direccionenvio_txt = p.String(), .cliente_Telefono_tel = p.String(maxLength:=200), .cliente_email_ema = p.String(maxLength:=200)},
+                                    body:="UPDATE [dbo].[tbl_empresa_cliente]
+                                    SET [empresa_id] = @empresa_id, [cliente_codigo_c25] = @cliente_codigo_c25, [cliente_nombre_nom] = @cliente_nombre_nom, [pais_id] = @pais_id, [cliente_direccionfacturacion_txt] = @cliente_direccionfacturacion_txt, [cliente_direccionenvio_txt] = @cliente_direccionenvio_txt, [cliente_Telefono_tel] = @cliente_Telefono_tel, [cliente_email_ema] = @cliente_email_ema
+                                    WHERE ([cliente_codigo_c25] = @cliente_codigo_c25)")
 
-                              DECLARE @ID int  
-                              SELECT @ID = [ID]  
-                              FROM [dbo].[Movies]  
-                              WHERE @@ROWCOUNT > 0 AND [ID] = scope_identity()  
-
-                              SELECT t0.[ID]  
-                              FROM [dbo].[Movies] AS t0  
-                              WHERE @@ROWCOUNT > 0 AND t0.[ID] = @ID")
         End Sub
 
         Public Overrides Sub Down()
-            DropStoredProcedure("dbo.Movie_Insert")
+            DropStoredProcedure("dbo.empresa_cliente_UpdateByCodigo")
         End Sub
 
     End Class
